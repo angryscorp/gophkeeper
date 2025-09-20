0cfaae927c5b0660171d2ea2ff08d68621baa80a
Anton Karpov <antony.karpov@icloud.com>
2025-09-20 18:57:12 +0200

    Refactor client entrypoint

--- client/cmd/main.go
+++ client/cmd/main.go
@@ -3,19 +3,10 @@ package main
 import (
 	"fmt"
 	"gophkeeper/client/internal/config"
-	"gophkeeper/client/internal/grpc/auth"
 	"gophkeeper/client/internal/repository/migration"
-	tokenrepo "gophkeeper/client/internal/repository/tokens/impl"
-	"gophkeeper/client/internal/tui/menu"
-	usecaseAuth "gophkeeper/client/internal/usecase/auth"
 	"log"
-	"os"
 
 	_ "github.com/mattn/go-sqlite3"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-
-	tea "github.com/charmbracelet/bubbletea"
 )
 
 const (
@@ -27,45 +18,25 @@ func main() {
 	// config
 	cfg := config.Config{
 		DatabaseDSN: fmt.Sprintf("file:%s?_pragma=busy_timeout=%d", dbName, busyTimeout),
+		ServerAddr:  "localhost:8443",
 		Debug:       true,
 	}
 
-	// migration
+	// Database migrations
 	if err := migration.MigrateSQLite(cfg.DatabaseDSN, "hexMasterKey"); err != nil {
 		panic(err)
 	}
 
-	// token repo
-	repo, closeDB, err := tokenrepo.New(cfg.DatabaseDSN, "hexMasterKey")
-	if err != nil {
-		panic(err)
-	}
+	// Bootstrap
+	program, closes := bootstrap(cfg)
+	defer func() {
+		for _, f := range closes {
+			f()
+		}
+	}()
 
-	defer closeDB()
-
-	fmt.Println(repo)
-
-	// grpc conn
-	conn, err := grpc.NewClient(
-		"localhost:8443",
-		grpc.WithTransportCredentials(insecure.NewCredentials()), // dial plaintext (dev)
-	)
-	if err != nil {
-		log.Fatalf("connect: %v", err)
-	}
-	defer func() { _ = conn.Close() }()
-
-	// grpc client
-	authClient := auth.New(conn)
-
-	// usecase
-	authUsecase := usecaseAuth.NewAuth(authClient)
-
-	// TUI
-	mainMenu := menu.New(func() *usecaseAuth.Auth { return authUsecase })
-	program := tea.NewProgram(mainMenu, tea.WithAltScreen())
+	// Run the program
 	if _, err := program.Run(); err != nil {
-		fmt.Printf("Something went wrong: %v", err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 }
--- client/internal/config/config.go
+++ client/internal/config/config.go
@@ -2,5 +2,6 @@ package config
 
 type Config struct {
 	DatabaseDSN string `env:"DATABASE_DNS"`
+	ServerAddr  string `env:"SERVER_ADDR"`
 	Debug       bool   `env:"DEBUG"`
 }
