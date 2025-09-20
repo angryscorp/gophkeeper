081c66a1cf1bc97c09169ee4b0233a246cff1d24
Anton Karpov <antony.karpov@icloud.com>
2025-09-20 16:31:03 +0200

    Add Graceful Shutdown for server

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,58 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"gophkeeper/pkg/grpc/auth"
-
-	authimpl "gophkeeper/server/internal/grpc/auth"
-	usecaseauth "gophkeeper/server/internal/usecase/auth"
-
 	"gophkeeper/server/internal/config"
 	"gophkeeper/server/internal/repository/migration"
-	usersrepo "gophkeeper/server/internal/repository/users/impl"
 	"log"
-	"net"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
 
 func main() {
-	fmt.Println("hello world - server")
-
 	cfg := config.Config{
 		DatabaseDSN: "postgres://db_user:db_password@postgres:5432/gophkeeper?sslmode=disable",
 		Debug:       true,
+		ServerAddr:  ":8443",
 	}
 
+	// Database migrations
 	if err := migration.MigratePostgres(cfg.DatabaseDSN); err != nil {
 		panic(err)
 	}
 
-	repo, closeDB, err := usersrepo.New(cfg.DatabaseDSN)
-	if err != nil {
-		panic(err)
-	}
+	// gRPC server initialization
+	grpcServer, closeFuncs := bootstrap(cfg)
+	defer func() {
+		for _, f := range closeFuncs {
+			f()
+		}
+	}()
 
-	defer closeDB()
-
-	// usecase
-	usecase := usecaseauth.New(repo)
-
-	// start gRPC server
-	lis, err := net.Listen("tcp", ":8443")
-	if err != nil {
-		panic(err)
-	}
-
-	grpcServer := grpc.NewServer()
-	auth.RegisterAuthServiceServer(grpcServer, authimpl.New(usecase))
-
-	// for debug
-	reflection.Register(grpcServer)
-
-	log.Printf("Auth gRPC server started")
-	if err := grpcServer.Serve(lis); err != nil {
-		panic(err)
+	// Run the server with graceful shutdown
+	if err := runWithGracefulShutdown(grpcServer, cfg.ServerAddr); err != nil {
+		log.Printf("Server error: %v", err)
 	}
 }
